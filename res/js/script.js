75179a7a46cc12179853484efde6094875a91b31
Aleksei Parm <alekseyparm@gmail.com>
2021-10-24 17:42:04 +0300

    Retrieve the posts info from the endpoint (URI)

--- res/js/script.js
+++ res/js/script.js
@@ -1,3 +1,4 @@
+//Posts from local JSON file
 $(function() {
     $.get("res/json/posts.json", function(posts) {
         for (post of posts) {
@@ -40,3 +41,48 @@ $(function() {
         }
     })
 });
+
+/*
+//Posts from the endpoint (URI)
+$(function() {
+    $.get("https://api.npoint.io/ea1ca018f1a8bf493ace", function (posts) {
+        for (post of posts) {
+            let div = $('<div class= "post">');
+
+
+            let divPostInfo = $('<div class= "post-info">');
+
+            let authorPhoto = null;
+            if (post.authorPhoto != null) {
+                authorPhoto = $('<img class="post-author-photo" src="' + post.authorPhoto + '">');
+            } else {
+                authorPhoto = $('<img class="post-author-photo" src="res/img/user.png">');
+            }
+
+            let date = $('<p class="date">').text(post.date);
+
+            divPostInfo.append(authorPhoto);
+            divPostInfo.append(date);
+
+
+            let image = null;
+            if (post.image != null) {
+                image = $('<img class="post-image" src="' + post.image + '">');
+            }
+
+            let text = $('<p class="post-text">').text(post.text);
+            let like = $('<button class="like-button">');
+
+
+            div.append(divPostInfo);
+            if (image != null) {
+                div.append(image);
+            }
+            div.append(text);
+            div.append(like);
+
+
+            $('.main-container').append(div)
+        }
+    })
+}); */
